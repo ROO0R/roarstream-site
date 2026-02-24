993ca2655dea85ad5b7cf4882e61bfc4ace1155d
ROO0R <164971120+ROO0R@users.noreply.github.com>
2026-02-24 09:36:08 -0500

    Add files via upload

---
+++ public/dylhb/cc.js
@@ -0,0 +1,3 @@
+document.write("<script src='http://somethingawful.crwdcntrl.net/o/cc.js'></script>");
+document.write("<script src='http://somethingawful.crwdcntrl.net/o/optimus.js'></script>");
+document.write("<script src='http://somethingawful.crwdcntrl.net/o/32.js'></script>");
