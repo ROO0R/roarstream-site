88e312255a2e167e57088129d5f08ba7e3d07a2c
ROO0R <164971120+ROO0R@users.noreply.github.com>
2026-02-24 09:34:18 -0500

    Delete dylhb directory

--- dylhb/1_files/cc.js
+++
@@ -1,3 +0,0 @@
-document.write("<script src='http://somethingawful.crwdcntrl.net/o/cc.js'></script>");
-document.write("<script src='http://somethingawful.crwdcntrl.net/o/optimus.js'></script>");
-document.write("<script src='http://somethingawful.crwdcntrl.net/o/32.js'></script>");
